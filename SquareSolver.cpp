dee90ab964099c6324e600f3e804b0568d85c893
Dmitry Tulkin <dmitrytjulkin@yandex.ru>
2025-08-23 16:38:01 +0300

    Some improvements

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -4,11 +4,14 @@
 #include <string.h>
 #include <float.h>
 
-/*this program solve quadratic equation*/
-int FinishProgram();
-int SolveQuadraticEquation(double, double, double, double*, double*);
-void ChooseCase(int, double, double);
-int DoubleComparison(double, double);
+/*this program solves quadratic equation*/
+
+int FinishProgram ();
+int SolveQuadraticEquation (double, double, double, double*, double*);
+void ChooseCase (int, double, double);
+int DoubleComparison (double, double);
+int InputCoefficients ();
+void CleanTheBuffer ();
 
 const int INF = -1;
 
@@ -16,20 +19,24 @@ int main ()
 {
     printf("You are solving the quadratic equation ax2 + bx + c = 0.\n");
     printf("Please, enter the coefficients a, b, c:\n");
+    InputCoefficients();
+    printf("You've finished the program\n");
+    return 0;
+}
+
+int InputCoefficients ()
+{
     double a = 0, b = 0, c = 0;  /*coefficients*/
     double x1 = 0, x2 = 0;   /*roots*/
     int i = 1;
     while (i != 0){
-        if (scanf ("%lf", &a) != 1 || scanf ("%lf", &b) != 1 || scanf ("%lf", &c) != 1){
+        if (scanf ("%lf", &a) != 1 || scanf ("%lf", &b) != 1 || scanf ("%lf", &c) != 1){    /*in case if user made a mistake*/
             printf("Incorrect input, try again:\n");
-            printf("Do you want to finish?\n YES/NO\n");
-            while(getchar() != '\n');
-            if (FinishProgram() == 1){
-                printf("You've finished the program.\n");
-                break;
-            }
+            printf("Do you want to finish?\nYES/NO\n");
+            if (FinishProgram() == 1)
+                return 0;
             continue;
-        } else {
+        } else {    /*if user input coefficients properly*/
             int NumOfRoots = 0;
             NumOfRoots = SolveQuadraticEquation(a, b, c, &x1, &x2);
             ChooseCase(NumOfRoots, x1, x2);
@@ -38,12 +45,13 @@ int main ()
     return 0;
 }
 
-int FinishProgram()
+int FinishProgram ()
 {
     char UsersAnswer[100] = " ";
     char ans1[] = "YES", ans2[] = "NO";
     int AnsMatches = 0;
     while (AnsMatches == 0){
+        CleanTheBuffer();
         scanf("%s", UsersAnswer);
         if (strcmp(UsersAnswer, ans1) == 0)
             return 1;
@@ -51,13 +59,14 @@ int FinishProgram()
             printf("Please, enter the coefficients a, b, c:\n");
             return 0;
         }
-        printf("Incorrect input, try again:\n");
-        while(getchar() != '\n');
+        printf("Incorrect input, try again.\n");
+        printf("Do you want to finish?\n");
+        printf("(only YES or NO answers)\n");
     }
     return 0;
 }
 
-int SolveQuadraticEquation(double a, double b, double c, double *x1, double *x2)
+int SolveQuadraticEquation (double a, double b, double c, double *x1, double *x2)
 {
     assert (isfinite (a));
     assert (isfinite (b));
@@ -93,7 +102,7 @@ int SolveQuadraticEquation(double a, double b, double c, double *x1, double *x2)
     return NumOfRoots;
 }
 
-void ChooseCase(int NumOfRoots, double x1, double x2){
+void ChooseCase (int NumOfRoots, double x1, double x2){
     if (NumOfRoots == INF)
         printf("This equation has infinite roots.\n");
     else if (NumOfRoots == 0)
@@ -104,7 +113,7 @@ void ChooseCase(int NumOfRoots, double x1, double x2){
         printf("This equation has two roots: x1 = %.6lf, x2 = %.6lf.\n", x1, x2);
 }
 
-int DoubleComparison(double a, double b)
+int DoubleComparison (double a, double b)
 {
     double diff = fabs(a - b);
     a = fabs(a);
@@ -114,4 +123,9 @@ int DoubleComparison(double a, double b)
         return 1;
     else
         return 0;
+}
+
+void CleanTheBuffer ()
+{
+    while(getchar() != '\n');
 }
